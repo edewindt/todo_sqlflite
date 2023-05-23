e60667f91ffa8113085b384449ff8008ea5ffaa4
Elias De Windt <edewindt27@outlook.com>
2023-05-22 20:55:52 -0700

    added an add area above list items

--- lib/todo_list.dart
+++ lib/todo_list.dart
@@ -17,6 +17,7 @@ class _TodoListState extends State<TodoList> {
         width: 800,
         child: ListView(
           children: [
+            Container(margin: EdgeInsets.symmetric(horizontal: 5),height: 40,color: Colors.lightGreen,child: Center(child:Text("Add")),),
             Card(
               elevation: 30,
               child: ListTile(
