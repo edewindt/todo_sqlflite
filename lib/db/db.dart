8530fc2626a236cca880df57e070de2a449876a6
Elias De Windt <edewindt27@outlook.com>
2023-05-29 11:53:02 -0700

    added database singleton

--- lib/db/db.dart
+++ lib/db/db.dart
@@ -1,3 +1,8 @@
+import 'package:path_provider/path_provider.dart';
+import "package:sqflite/sqflite.dart";
+import 'dart:io';
+import 'package:path/path.dart';
+
 class TodoDBItem {
   final int? id;
   final bool checked;
@@ -17,4 +22,23 @@ class TodoDBItem {
           'checked':checked,
         };
        }
+}
+
+class DBHelper {
+  DBHelper._privateConstructor();
+  static final DBHelper instance = DBHelper._privateConstructor();
+  static Database? _database;
+  Future<Database> get database async => _database ??= await _initDatabase();
+
+
+  Future<Database> _initDatabase() async {
+    Directory appdirectory = await getApplicationDocumentsDirectory();
+    String path = join(appdirectory.path, 'app.db');
+    return await openDatabase(path, version: 1, onCreate: _onCreate);
+  }
+
+  Future _onCreate(Database db, int version) async {
+    await db.execute('CREATE TABLE todos(id INTEGER PRIMARY KEY, content TEXT, checked BOOL)');
+  }
+
 }
