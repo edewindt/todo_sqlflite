133bf93c455ded8f5fee6d270be4b7996850e11a
Elias De Windt <edewindt27@outlook.com>
2023-05-29 11:42:46 -0700

    finished the sqflite model

--- lib/db/db.dart
+++ lib/db/db.dart
@@ -3,4 +3,18 @@ class TodoDBItem {
   final bool checked;
   final String content;
   TodoDBItem({this.checked = false, this.id, required this.content});
+
+  factory TodoDBItem.fromMap(Map<String, dynamic> json) => TodoDBItem(
+      id: json['id'], 
+      content: json['content'],
+       checked: json['checked']
+       );
+
+       Map<String,dynamic> toMap() {
+        return {
+          'id':id,
+          'content':content,
+          'checked':checked,
+        };
+       }
 }
